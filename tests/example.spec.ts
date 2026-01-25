d3a197eb13353d2ef17c6c2eb32988fcc8e3f32d
Raghuraman <ragu123@aol.in>
2026-01-25 22:50:24 +0530

    making test fail again

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -11,8 +11,8 @@ test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('link', { name: '' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installatio' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
